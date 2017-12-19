ef35ee647b54ce2c3b13edf6cd5aff0012de404a
Volodymyr <volodymyr482@gmail.com>
2017-12-19 13:48:30 +0200

    Add project files.

---
+++ SoftServeITAcademy_Homework_3D/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftServeITAcademy_Homework_3D
+{
+  public  class Program
+    {
+      public static void Main(string[] args)
+        {
+            // Task 1
+            int i;
+            double a, b, suma = 0;
+            b = 1;
+            for ( i = 1; i <= 30; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    a = i / 2;
+                }
+                else
+                {
+                    a = i;
+                }
+
+                suma = (a - b) * (a - b);
+            }
+
+            Console.WriteLine("Suma= {0}", suma);
+
+            // Task 2
+            Console.Write("Enter N=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (i = 0; i < n; i++)
+            {
+                        Console.Write(" {0} ", i);
+            }
+
+            Console.WriteLine("  ");
+             for (i = 0; i < n; i++)
+            {
+                sum += i;
+                Console.Write(" {0} ", sum);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
